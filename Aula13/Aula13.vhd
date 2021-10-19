3233654cb8734c7825fadb2ddf893d2587340722
Gabriel Kabbani <gabrielmk@al.insper.edu.br>
2021-10-19 12:59:15 -0300

    infra com erros

---
+++ Aula13/Aula13.vhd
@@ -0,0 +1,69 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Aula13 is
+  -- Total de bits das entradas e saidas
+  generic ( larguraDados : natural := 32;
+				larguraUm : natural := 1;
+        simulacao : boolean := TRUE -- para gravar na placa, altere de TRUE para FALSE
+  );
+  port   (
+    CLOCK_50 : in std_logic;
+	 
+	 OP_ULA : in std_logic;
+	 WR_Banco : in std_logic;
+	 INSTR : out std_logic_vector(6 downto 0)
+
+  );
+end entity;
+
+
+architecture arquitetura of Aula13 is
+
+	signal CLK : std_logic;
+	signal saidaSOM : std_logic_vector(larguraDados-1 downto 0);
+	signal PC_out : std_logic_vector(larguraDados-1 downto 0);
+	signal ROM_instru : std_logic_vector(larguraDados-1 downto 0);
+	signal Saida_ULA : std_logic_vector(larguraDados-1 downto 0);
+	signal entradaAULA : std_logic_vector(larguraDados-1 downto 0);
+	signal entradaBULA : std_logic_vector(larguraDados-1 downto 0);
+
+
+begin
+
+-- Instanciando os componentes:
+
+-- clock configurando como borda de subida
+CLK <= CLOCK_50;
+INSTR <= ROM_instru;
+
+PC : entity work.registradorGenerico   generic map (larguraDados => 32)
+        port map (DIN => saidaSOM, DOUT => PC_out, ENABLE => '1', CLK => CLK, RST => '0');
+			 
+somador :  entity work.somaConstante  generic map (larguraDados => 32, constante => 1)
+        port map( entrada => PC_out, saida => saidaSOM);
+		  
+-- ROM com instrucoes atualizadas
+ROM : entity work.ROMMIPS  generic map (memoryAddrWidth => 32, dataWidth => 32, addrWidth => 32)
+        port map (clk => CLK, Endereco => PC_out, Dado => ROM_instru);
+			 
+Banco_Registradores : entity work.bancoReg generic map (larguraDados => 32, larguraEndBancoRegs => 5)
+		  port map (
+		  
+				enderecoA => ROM_instru(25 downto 21),
+				enderecoB => ROM_instru(20 downto 16),
+				enderecoC => ROM_instru(15 downto 11),
+				escreveC => WR_Banco,
+				dadoEscritaC => Saida_ULA,
+				saidaA => entradaAULA,
+				saidaB => entradaBULA
+		  );
+		  
+		  
+ULA : entity work.ULASomaSub  generic map(larguraDados => larguraDados)
+          port map (entradaA => entradaAULA, entradaB => entradaBULA, saida => Saida_ULA, seletor => OP_ULA, flag_zero  => ------);
+
+
+
+
+end architecture;
---
+++ Aula13/ULASomaSub.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;    -- Biblioteca IEEE para funções aritméticas
+
+entity ULASomaSub is
+    generic ( larguraDados : natural := 4 );
+    port (
+      entradaA, entradaB:  in STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+      seletor:  in STD_LOGIC_vector (1 downto 0);
+      saida:    out STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+		flag_zero : out STD_LOGIC
+    );
+end entity;
+
+architecture comportamento of ULASomaSub is
+   signal soma :      STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+   signal subtracao : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+	signal resultado : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+	constant valor_zero : STD_LOGIC_VECTOR((larguraDados-1) downto 0) := (others => '0');
+    begin
+      soma      <= STD_LOGIC_VECTOR(unsigned(entradaA) + unsigned(entradaB));
+      subtracao <= STD_LOGIC_VECTOR(unsigned(entradaA) - unsigned(entradaB));
+		
+		
+		
+      resultado <= soma when (seletor = "00") else
+					subtracao when (seletor = "01") else
+					entradaB;
+		
+					
+		flag_zero <= '1' when resultado = valor_zero else '0';
+		saida <= resultado;
+		
+		
+end architecture;
